6512f82149a9bdb8d40e1462d70bbabadf5877f7
Britt Breugelmans <breugelmans.britt@gmail.com>
2026-03-15 22:57:27 +0100

    connection to sql established

--- Program.cs
+++ Program.cs
@@ -1,1 +1,16 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using MySql.Data.MySqlClient;
+
+namespace mySql
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MySqlConnection connection = new MySqlConnection("server=localhost;user=root;password=Spot1234"); //localhost = ip adress server (my computer)
+            connection.Open();
+
+            Console.WriteLine("Successfully connected to MySql server");
+
+        }
+    }
+}
